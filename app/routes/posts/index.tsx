019e7bb2a4927d51113171be6aca350801c434a6
Ian Boyle <irboyle21@gmail.com>
2023-02-27 19:16:09 -0700

    adds useloader, json

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,7 +1,35 @@
+import { Link, useLoaderData } from "@remix-run/react"
+import {json} from '@remix-run/node'
+export const loader = async () => {
+
+  const posts = [
+    {
+    slug: 'my-first-post',
+    title: 'My First Post'
+    },
+    {
+    slug: 'my-second-post',
+    title: 'My Second Post'
+    }
+  ]
+  return json({posts})
+}
 export default function PostsRoute() {
+  const {posts} = useLoaderData();
+
+
   return (
     <main>
       <h1>Posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.slug}>
+            <Link to={post.slug} className="text-blue-600 underline"> 
+            {post.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </main>
   )
 }
