78019ab8b1bd18891bd92bbb98534fbfc82f7844
Ian Boyle <irboyle21@gmail.com>
2023-02-27 20:15:46 -0700

    over fetching

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,5 +1,13 @@
 import {prisma} from '~/db.server'
 
+export async function getPostListings() {
+  return prisma.post.findMany({
+    select:{
+      slug: true,
+      title: true,
+    }
+  })
+}
 export async function getPosts() {
   return prisma.post.findMany();
 }
