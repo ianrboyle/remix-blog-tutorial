e1d301f16269929cbf5174455a832996520d5967
Ian Boyle <irboyle21@gmail.com>
2023-03-11 09:03:18 -0500

    adds typechecking

--- app/routes/posts/admin/$slug.tsx
+++ app/routes/posts/admin/$slug.tsx
@@ -1,19 +1,27 @@
+
+import { Post } from "@prisma/client";
 import type { ActionArgs, ActionFunction, LoaderFunction} from "@remix-run/node";
 import { json} from "@remix-run/node";
 import { redirect } from "@remix-run/node";
 import { Form, useActionData, useLoaderData, useNavigation } from "@remix-run/react";
 import invariant from "tiny-invariant";
+
 import { createPost, deletePost, getPost, updatePost } from "~/models/post.server";
 import { requireAdminUser } from "~/session.server";
+type LoaderData = {post?: Post}
+
 
 export const loader: LoaderFunction = async ({ request, params }) => {
   await requireAdminUser(request);
+  invariant(params.slug, 'slug is required')
   if (params.slug === 'new') {
-    return json({})
+    return json<LoaderData>({})
   }
   const post = await getPost(params.slug)
-  return json({post})
+  return json<LoaderData>({post})
 }
+
+
 
 type ActionData = {
   title: null | string,
@@ -25,6 +33,7 @@ export const action: ActionFunction = async ({ request, params } : ActionArgs) =
   await new Promise((res) => setTimeout(res, 1000));
   const formData = await request.formData();
   const intent = formData.get('intent')
+  invariant(params.slug, 'slug is required')
   if (intent === 'delete') {
     await deletePost(params.slug)
     return redirect('/posts/admin')
@@ -68,7 +77,7 @@ export const action: ActionFunction = async ({ request, params } : ActionArgs) =
 const inputClassName = `w-full rounded border border-gray-500 px-2 py-1 text-lg`;
 
 export default function NewPost() {
-  const data = useLoaderData();
+  const data = useLoaderData() as LoaderData;
   const errors = useActionData() as ActionData;
 
   const navigation = useNavigation();
